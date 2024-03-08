c8c8e33088ec85546286f7bc265a59e02ebf8fe4
longbiao_wu <615944323@qq.com>
2024-03-08 14:31:18 +0800

    feature: change the content classify

--- src/components/SubNav.vue
+++ src/components/SubNav.vue
@@ -17,7 +17,7 @@ const route = useRoute()
 
     <div mb-0 flex="~ col gap-1 sm:row sm:gap-3 wrap" text-3xl>
       <RouterLink to="/posts" class="!border-none" :class="route.path === '/posts' ? activeStyle : inactiveStyle">
-        Blog
+        博客
       </RouterLink>
       <RouterLink to="/notes" class="!border-none" :class="route.path === '/notes' ? activeStyle : inactiveStyle">
         笔记
