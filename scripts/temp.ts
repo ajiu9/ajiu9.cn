7820bff325b0bd197c2d55c69dd58f1ba254320e
longbiao_wu <615944323@qq.com>
2024-03-15 09:48:00 +0800

    chore: add dist

--- scripts/temp.ts
+++ scripts/temp.ts
@@ -6,15 +6,10 @@ const __dirname = path.resolve()
 function copyAndPushToOrigin(sourceDir: string, targetDir: string) {
   try {
     execSync(`cp -r ${sourceDir} ${targetDir}`)
-
     process.chdir(targetDir)
-
     execSync('git init')
-
     execSync('git add .')
-
     execSync('git commit -m "chore: blog"')
-
     execSync('git remote add origin https://github.com/ajiu9/ajiu9.github.io')
     execSync('git branch -M gh-pages')
     execSync('git push --force origin gh-pages')
