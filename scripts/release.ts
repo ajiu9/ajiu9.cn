171df6b1a6f513226cbe12357a0170b40953c35f
longbiao_wu <615944323@qq.com>
2024-04-07 17:22:13 +0800

    chore:  release

--- scripts/release.ts
+++ scripts/release.ts
@@ -10,14 +10,6 @@ function buildAndPush() {
     execSync('git add .', { stdio: 'inherit' })
     execSync('git commit -m "chore: build"', { stdio: 'inherit' })
     execSync('git push', { stdio: 'inherit' })
-    // execSync(`cp -r ${sourceDir} ${targetDir}`)
-    // process.chdir(targetDir)
-    // execSync('git init')
-    // execSync('git add .')
-    // execSync('git commit -m "chore: blog"')
-    // execSync('git remote add origin https://github.com/ajiu9/ajiu9.github.io')
-    // execSync('git branch -M gh-pages')
-    // execSync('git push --force origin gh-pages')
     console.log('Successfully pushed to origin.')
   }
   catch (error) {
