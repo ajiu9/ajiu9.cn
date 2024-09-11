dafaea532f217d4b6f6bbe2ed7620c6bd65fc109
ajiu9 <615944323@qq.com>
2024-09-11 22:26:48 +0800

    chore: build

--- dist/index.js
+++
@@ -1,1 +0,0 @@
-window.screenOrientation="sensor_landscape",loadLib("libs/babel-require.js"),loadLib("libs/laya.core.js"),loadLib("libs/laya.ui.js"),loadLib("libs/laya.d3.js"),loadLib("js/main.js");
