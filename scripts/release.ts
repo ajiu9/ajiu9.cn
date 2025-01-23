0f5a28448db6c1db3aaa49ed8f62cef8087505e0
longbiao_wu <615944323@qq.com>
2025-01-23 10:07:16 +0800

    chore: remove unuse pkg

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,15 +1,12 @@
 import { execSync } from 'node:child_process'
 
-// import path from 'node:path'
-
-// const __dirname = path.resolve()
-
 function buildAndPush() {
   try {
     execSync('pnpm build', { stdio: 'inherit' })
     execSync('git add .', { stdio: 'inherit' })
     execSync('git commit -m "chore: build"', { stdio: 'inherit' })
     execSync('git push', { stdio: 'inherit' })
+    // eslint-disable-next-line no-console
     console.log('Successfully pushed to origin.')
   }
   catch (error) {
